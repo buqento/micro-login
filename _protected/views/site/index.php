e41fb5164df39e9441639a8312cf2605f6b92922
buqento <buqento@gmail.com>
2018-12-14 14:57:54 +0700

    update

---
+++ _protected/views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Hi Admin!</h1>
+
+        <p class="lead">Selamat datang kembali</p>
+    </div>
+
+
+    </div>
+</div>
