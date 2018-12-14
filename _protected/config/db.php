b07a9661b755cd39b68be3e70fc8adcd33983dac
buqento <buqento@gmail.com>
2018-12-14 15:24:17 +0700

    update

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -2,8 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=micro',
-    'username' => 'root',
+    // 'dsn' => 'mysql:host=localhost;dbname=micro',
+    // 'username' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=timoerin_login',
+    'username' => 'timoerin_bvqento',
     'password' => 'Manggureb3!',
     'charset' => 'utf8',
 
